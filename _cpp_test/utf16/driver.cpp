0d51a3e305b26c54cacef11a1ed2c540ed827680
kuro11pow2 <kuro11pow2@gmail.com>
2021-09-29 21:44:15 +0900

    Add driver code for native code execution.
    _cpp_test directory should not be merged with the master branch.

---
+++ _cpp_test/utf16/driver.cpp
@@ -0,0 +1,5 @@
+//
+// Created by kuro1 on 21-09-29.
+//
+
+#include "driver.h"
---
+++ _cpp_test/utf16/driver.h
@@ -0,0 +1,14 @@
+//
+// Created by kuro1 on 21-09-29.
+//
+
+#ifndef EUPHONY_DRIVER_H
+#define EUPHONY_DRIVER_H
+
+
+class driver {
+
+};
+
+
+#endif //EUPHONY_DRIVER_H
